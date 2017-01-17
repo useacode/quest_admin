7f4733303047935ebe0c1b787d789c48e050bd6c
Ivan Mikhaylov <Ivan Mikhaylov>
2017-01-17 11:44:31 +0300

    first commit

---
+++ orders.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Квест</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="/admin/assets/css/bootstrap.css">
+</head>
+<body>
+    <div class="header">
+       <div class="">
+           <div class="clearfix">
+                <div class="col-xs-11">
+                    <nav class="top-menu navbar">
+                       <div class="navbar-header">
+                            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target='.navbar-collapse'>
+                                <span class="sr-only">Toggle navigation</span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+                        </div>
+                        <div class="collapse navbar-collapse" id="collapse">
+                            <ul class="nav navbar-nav">
+                                <li class="nav-item"><a href="#about"><nob>Новые заказы</nob></a></li>
+                                <li class="nav-item"><a href="#photo">График</a></li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+           </div>
+        </div>                
+    </div>     
+    <div class="orders">
+        <div class="caption">
+            <h2>~ Новые заказы ~</h2>
+        </div>
+        <div class="order">
+            <span class="order__name">Михайлов Иван</span>
+            <span class="order__phone">+79210000000</span>
+            <span class="order__date">21.01.2017</span>
+            <div class="">
+                <div class="btn btn-default btn-order">Подтвердить</div>
+                <div class="btn btn-default btn-order">Назначить время</div>
+                <div class="btn btn-default btn-order">Удалить</div>
+            </div>
+            <hr>
+        </div>
+        <div class="order">
+            <span class="order__name">Михайлов Иван</span>
+            <span class="order__phone">+79210000000</span>
+            <span class="order__date">21.01.2017</span>
+            <div class="">
+                <div class="btn btn-default btn-order">Подтвердить</div>
+                <div class="btn btn-default btn-order">Назначить время</div>
+                <div class="btn btn-default btn-order">Удалить</div>
+            </div>
+            <hr>
+        </div>
+        <div class="order">
+            <span class="order__name">Михайлов Иван</span>
+            <span class="order__phone">+79210000000</span>
+            <span class="order__date">21.01.2017</span>
+            <div class="">
+                <div class="btn btn-default btn-order">Подтвердить</div>
+                <div class="btn btn-default btn-order">Назначить время</div>
+                <div class="btn btn-default btn-order">Удалить</div>
+            </div>
+            <hr>
+        </div>
+
+    </div>
+</body>
+</html>
